1fc1f6e3f0dd777f8cde0b6c05f2b6fc7e56fcb5
ANDR6709 <andreatovar549@gmail.com>
2024-01-27 22:58:18 -0500

    ejercicio calculadora con errores corregidos

--- ejercicio 1/calculadora.py
+++ ejercicio 1/calculadora.py
@@ -4,16 +4,43 @@ def calculadora():
     operacion = input("Ingrese la operación (+, -, *, /): ")
 
     if operacion == '+':
-        resultado = num + num2
+        resultado = num + num2 # error: num no esta definida como variable num 1
     elif operacion == '-':
         resultado = num1 - num2
     elif operacion == '*':
         resultado = num1 * num2
     elif operacion == '/':
-        resultado = num / num2
+        resultado = num / num2  # error: num no esta definida como variable num 1
     else:
         resultado = "Operación no válida"
 
-    print("Resultado: "resultado)
+    print("Resultado: "resultado)  # error: falta una coma despues de cerrar comillas
 
-calculdora()
+calculdora()                       #error: falta definir calculadora como funcion y faltan los dos puntos
+
+
+
+# correcciones
+
+
+def calculadora():
+    num1 = float("Ingrese el primer número: ")
+    num2 = float("Ingrese el segundo número: ")
+    operacion = input("Ingrese la operación (+, -, *, /): ")
+
+    if operacion == '+':
+        resultado = num1 + num2 # correccion: se define num como variable num 1
+    elif operacion == '-':
+        resultado = num1 - num2
+    elif operacion == '*':
+        resultado = num1 * num2
+    elif operacion == '/':
+        resultado = num1 / num2  # correccion: se define num como variable num 1
+    elif operacion == '-':
+        resultado = "Operación no válida"
+
+    print("Resultado: ",resultado)  # correccion: se coloca la coma despues de cerrar comillas
+
+def calculdora():                       #correccion: se define calculadora como funcion y se ponen los dos puntos
+
+
