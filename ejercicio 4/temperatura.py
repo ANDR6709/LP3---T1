ace5217a150dc60a78812f1c584c3ebdf9313e0f
ANDR6709 <andreatovar549@gmail.com>
2024-01-29 09:40:02 -0500

    Ejercicio temperatura.py con errores corregidos

--- ejercicio 4/temperatura.py
+++ ejercicio 4/temperatura.py
@@ -1,6 +1,19 @@
 def celsius_a_fahrenheit(celsius)
-    return (celsius * 9/5)  32
+    return (celsius * 9/5)   32     # Error: falta el signo + despues del parentesis que se cierra
 
 temperatura_celsius = float("Ingrese la temperatura en Celsius: ")
 temperatura_fahrenheit = celsius_a_fahrenheit(temperatura_celsius)
 print(f"{temperatura_celsius}°C es igual a {temperatura_fahrenheit}°F")
+
+
+# Correccion:
+
+def celsius_a_fahrenheit(celsius)
+    return (celsius * 9/5)  + 32     # Correccion: Se agrega el signo + despues del parentesis que se cierra
+
+temperatura_celsius = float("Ingrese la temperatura en Celsius: ")
+temperatura_fahrenheit = celsius_a_fahrenheit(temperatura_celsius)
+print(f"{temperatura_celsius}°C es igual a {temperatura_fahrenheit}°F")
+
+
+
