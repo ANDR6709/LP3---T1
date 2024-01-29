3cec3cc0f3fe2ad2c01f63fcf1d45efada7ab166
ANDR6709 <andreatovar549@gmail.com>
2024-01-29 10:23:07 -0500

    Ejercicio palindromo.py con errores corregidos

--- ejercicio 6/palindromo.py
+++ ejercicio 6/palindromo.py
@@ -1,4 +1,4 @@
-def es_palindromo(texto)
+def es_palindromo(texto) #Error: faltan los dos puntos
     texto = ''.join(caracter.lower() for caracter in texto if caracter.isalnum())
     return texto == texto[::-1]
 
@@ -7,3 +7,20 @@ if es_palindromo():
     print(f"{palabra_frase} es un palíndromo.")
 else:
     print(f"{palabra_frase} no es un palíndromo.")
+
+
+    # Correccion:
+
+    def es_palindromo(texto): # Correccion: Se agregan  los dos puntos
+        texto = ''.join(caracter.lower() for caracter in texto if caracter.isalnum())
+        return texto == texto[::-1]
+
+palabra_frase = input("Ingrese una palabra o frase: ")
+if es_palindromo():
+    print(f"{palabra_frase} es un palíndromo.")
+else:
+    print(f"{palabra_frase} no es un palíndromo.")
+
+
+
+    
