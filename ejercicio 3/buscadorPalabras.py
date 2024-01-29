2f5cc76bb0c7ec8a114b1fdddb3b0cda1f1bc86f
ANDR6709 <andreatovar549@gmail.com>
2024-01-29 08:11:49 -0500

    ejercicio contador de palabras con errores corregidos

--- ejercicio 3/buscadorPalabras.py
+++ ejercicio 3/buscadorPalabras.py
@@ -4,4 +4,23 @@ def contar_palabra(texto, palabra)
 texto = "Este es un ejemplo de texto. Este texto tiene palabras repetidas."
 palabra_buscada = "texto"
 
-print(f"La palabra '{palabra_buscada aparece contar_palabra(texto, palabra_buscada)} veces.")
+print(f"La palabra '{palabra_buscada aparece contar_palabra(texto, palabra_buscada)} veces.")  
+                  # Error: en la variable palabra_buscada falta cerrar el  corchete
+                  # Error: en el string aparece faltan comillas simples 
+                  # Error: En la funcion contar palabra falta abrir el corchete
+
+
+# Correcciones:
+
+
+def contar_palabra(texto, palabra)
+    return texto.lower().split().count(palabra.lower())
+
+texto = "Este es un ejemplo de texto. Este texto tiene palabras repetidas."
+palabra_buscada = "texto"
+
+print(f"La palabra '{palabra_buscada} 'aparece' {contar_palabra(texto, palabra_buscada)} veces.")  
+                                                                                        
+# Correccion: En la variable palabra_buscada se cierra el corchete
+# Correccion: En el string aparece se colocan comillas simples
+# Correccion: En la funcion contar palabra se abre el corchete
